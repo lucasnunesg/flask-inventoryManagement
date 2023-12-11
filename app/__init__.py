18a318d60524e9fd2df70363e5b5501fb24fd5e4
Lucas Nunes <lucas.nunesgomes1@gmail.com>
2023-12-11 10:56:09 -0300

    moved flask configs to specific file

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,15 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///storage.db"
+
+app.config.from_object("config")
+
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
 
 from app.controllers import default
 
